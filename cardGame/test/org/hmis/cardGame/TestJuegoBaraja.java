bcb098705bce396ea18f138ac74c4e2d90afb16a
Adrian Camacho Capilla <acc728@inlumine.ual.es>
2022-05-29 20:39:08 +0200

    Tests simples de baraja

--- cardGame/test/org/hmis/cardGame/TestJuegoBaraja.java
+++ cardGame/test/org/hmis/cardGame/TestJuegoBaraja.java
@@ -7,8 +7,30 @@ import org.junit.jupiter.api.Test;
 class TestJuegoBaraja {
 
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testRobarCarta() {
+		Deck baraja = new Deck();
+		int cartasIniciales = baraja.cardsLeft();
+		baraja.dealCard();
+		int cartasTrasRobar = baraja.cardsLeft();
+		assertNotEquals(cartasIniciales, cartasTrasRobar);
 	}
-
+	
+	@Test
+	void testShuffleBaraja() {
+		Deck baraja = new Deck();
+		String barajaInicial = baraja.toString();
+		baraja.shuffle();
+		String barajaTrasMezclar = baraja.toString();
+		assertNotEquals(barajaInicial, barajaTrasMezclar);
+	}
+	
+	@Test
+	void testNotNullCard() {
+		Deck baraja = new Deck();
+		Card carta = baraja.dealCard();
+		int suit = carta.getSuit();
+		int value= carta.getValue();
+		assertNotNull(suit);
+		assertNotNull(value);
+	}
 }
